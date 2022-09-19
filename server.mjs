968ef95d7594da0ae2f53a48e083c401c92a9f1c
M.Inzamam Malik <malikasinger1@gmail.com>
2022-09-19 20:02:14 +0500

    abc

--- server.mjs
+++ server.mjs
@@ -4,7 +4,8 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     console.log('response from sir inzamam computer: ' +  req.ip);
-    console.log("come changes");
+    console.log("some changes");
+    console.log("some some changes");
     res.send('response from sir inzamam computer: ' +  req.ip)
 })
 
